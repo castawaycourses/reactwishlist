6730895843762b7f81d8a4ad3b66dbb40bb5741f
Goyo Regalado <goyoregalado@gmail.com>
2022-09-12 22:40:33 +0100

    wip(TodoForm): Working on TodoForm functionality

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react"
+
 const TodoForm = () => {
-  return (
-    <div>TodoForm</div>
-  )
+    const [taskName, setTaskName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(taskName);
+        setTaskName("");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="todoList">
+                <div className="taskInput">
+                    <input 
+                        type="text"
+                        className="text"
+                        value={taskName}
+                        onChange={(e) => setTaskName(e.target.value)}
+                    />
+                </div>
+                <button 
+                    className="button pink"
+                    disabled={taskName ? "": "disabled"}>
+                    Add
+                </button>
+
+            </div>
+        </form>
+    )
 }
 
 export default TodoForm
